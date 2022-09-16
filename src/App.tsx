91731d400541af137d9791112692f3e2d8b27511
Uhmopt <qethum@gmail.com>
2022-09-17 00:52:58 +0900

    [New] redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { ThemeProvider } from "@mui/material";
+import { Provider } from "react-redux";
 import "./App.css";
 import Root from "./routes";
+import store from "./store";
 import { lightTheme } from "./theme/muiThemes";
 
 function App() {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <Root />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={lightTheme}>
+        <Root />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
