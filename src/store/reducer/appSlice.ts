46f1ca0a207b8cf7f343728f9ac6b2d8e0738d8d
Uhmopt <qethum@gmail.com>
2022-11-25 11:25:40 +0900

    [New] store

--- src/store/reducer/appSlice.ts
+++ src/store/reducer/appSlice.ts
@@ -1,29 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  value: 0,
+};
+
+type InitialType = typeof initialState;
 
 export const appSlice = createSlice({
-  name: "app",
-  initialState: {
-    value: 0,
-  },
+  name: 'app',
+  initialState: initialState,
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1;
+    setApp: (
+      state,
+      action: {
+        payload:
+          | Partial<InitialType>
+          | ((curState: InitialType) => InitialType);
+      }
+    ) => {
+      if (typeof action.payload === 'function') {
+        return action.payload(state);
+      }
+      return { ...state, ...action.payload };
     },
-    decrement: (state) => {
-      state.value -= 1;
-    },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload;
+
+    resetApp: () => {
+      return initialState;
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = appSlice.actions;
+export const { setApp, resetApp } = appSlice.actions;
 
 export type AppState = ReturnType<typeof appSlice.getInitialState>;
 
